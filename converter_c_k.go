adcf7a94a7df7b5779c7396f9e98d416b3a8289e
marcio-cavalcante <marcio_acc@hotmail.com>
2024-12-14 17:51:43 -0300

    Add files via upload

---
+++ converter_c_k.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	pontoEbulicaoEmGrausCelsius := 100
+	temperaturaDeEbulicaoEmKelvin := 273 + pontoEbulicaoEmGrausCelsius
+
+	fmt.Println("O ponto de ebulição da água em K é:", temperaturaDeEbulicaoEmKelvin)
+}
